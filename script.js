90d44822c3a04ee48c7fac6bfbc0fd8a33b86b6a
pjaworski1402 <p.jaworski1402@gmail.com>
2021-05-16 21:39:11 +0200

    scale option

--- script.js
+++ script.js
@@ -9,27 +9,55 @@ const canvas = document.querySelector(".app__canvas");
 let context = canvas.getContext("2d");
 let img = new Image();
 img.crossOrigin = "anonymous";
-let imageUrl = "";
+let imageData = "";
+
+const render = () => {
+  img.src = imageData;
+};
+
 const imageSettings = {
   degrees: 0,
   previousDegree: 0,
   flip: [1, 1],
   previousFlip: [1, 1],
-  previousValue: (tool) => {
+  resolution: [0, 0],
+  previousResolution: [0, 0],
+  previousValue(tool) {
     tool = tool.toUpperCase();
     switch (tool) {
       case "ROTATE":
-        imageSettings.degrees = imageSettings.previousDegree;
+        this.degrees = this.previousDegree;
         break;
       case "FLIP":
-        {
-          imageSettings.flip[0] = imageSettings.previousFlip[0];
-          imageSettings.flip[1] = imageSettings.previousFlip[1];
-        }
+        this.flip = [...this.previousFlip];
+        break;
+      case "SCALE":
+        this.resolution = [...this.previousResolution];
         break;
       default:
         break;
     }
+  },
+  rotate(value, input) {
+    this.degrees = value;
+    input.value = value;
+    render();
+  },
+  doFlip(orientation) {
+    if (orientation === "horizontal") {
+      this.flip[0] *= -1;
+    } else if (orientation === "vertical") {
+      this.flip[1] *= -1;
+    }
+    render();
+  },
+  scale({ width, height }) {
+    if (typeof width !== "undefined") {
+      this.resolution[0] = width;
+    } else if (typeof height !== "undefined") {
+      this.resolution[1] = height;
+    }
+    render();
   },
 };
 
@@ -57,10 +85,6 @@ const testImage = (url, callback) => {
   }, timeout);
 };
 
-const render = () => {
-  img.src = imageUrl;
-};
-
 const toggleTools = (imageIsLoaded) => {
   if (imageIsLoaded) {
     tools.style.display = "initial";
@@ -83,11 +107,16 @@ const downloadPicture = () => {
   a.click();
 };
 
-const displayImage = (src) => {
+const displayImage = (base64) => {
   hideUploadUI();
+  let isInitialized = false;
   img.onload = () => {
-    canvas.width = img.width;
-    canvas.height = img.height;
+    if (!isInitialized) {
+      imageSettings.resolution = [img.width, img.height];
+      isInitialized = true;
+    }
+    canvas.width = imageSettings.resolution[0];
+    canvas.height = imageSettings.resolution[1];
     context.scale(imageSettings.flip[0], imageSettings.flip[1]);
     context.translate(
       (canvas.width / 2) * imageSettings.flip[0],
@@ -96,24 +125,10 @@ const displayImage = (src) => {
     context.rotate((imageSettings.degrees * Math.PI) / 180);
     context.drawImage(img, -img.width / 2, -img.height / 2);
   };
-  imageUrl = src;
+  imageData = base64;
   render();
 };
 
-const rotate = (value, input) => {
-  imageSettings.degrees = value;
-  input.value = value;
-  render();
-};
-
-const flip = (orientation) => {
-  if (orientation === "horizontal") {
-    imageSettings.flip[0] *= -1;
-  } else if (orientation === "vertical") {
-    imageSettings.flip[1] *= -1;
-  }
-  render();
-};
 const renderTool = (tool, toolName) => {
   const quitFromTool = () => {
     render();
@@ -159,21 +174,23 @@ const rotateTool = (toolWrapper) => {
 
   setInputAttributes(inputRange, "range", "app__rotateRange");
   setInputAttributes(input, "number", "app__rotateValue");
-  inputRange.addEventListener("change", (e) => rotate(e.target.value, input));
+  inputRange.addEventListener("change", (e) =>
+    imageSettings.rotate(e.target.value, input)
+  );
   inputRange.addEventListener("mousemove", (e) =>
-    rotate(e.target.value, input)
+    imageSettings.rotate(e.target.value, input)
   );
   inputRange.addEventListener("touchmove", (e) =>
-    rotate(e.target.value, input)
+    imageSettings.rotate(e.target.value, input)
   );
-  input.addEventListener("change", (e) => rotate(e.target.value, inputRange));
+  input.addEventListener("change", (e) =>
+    imageSettings.rotate(e.target.value, inputRange)
+  );
   toolWrapper.append(inputRange, input);
 };
 
 const flipTool = (toolWrapper) => {
-  imageSettings.previousFlip[0] = imageSettings.flip[0];
-  imageSettings.previousFlip[1] = imageSettings.flip[1];
-  const title = document.createElement("span");
+  imageSettings.previousFlip = [...imageSettings.flip];
   const flipVertButton = document.createElement("button");
   const flipHorizButton = document.createElement("button");
   const flipIcoVert = document.createElement("img");
@@ -187,9 +204,41 @@ const flipTool = (toolWrapper) => {
   flipIcoHoriz.style.margin = "auto";
   flipVertButton.appendChild(flipIcoVert);
   flipHorizButton.appendChild(flipIcoHoriz);
-  flipVertButton.addEventListener("click", () => flip("vertical"));
-  flipHorizButton.addEventListener("click", () => flip("horizontal"));
-  toolWrapper.append(title, flipVertButton, flipHorizButton);
+  flipVertButton.addEventListener("click", () =>
+    imageSettings.doFlip("vertical")
+  );
+  flipHorizButton.addEventListener("click", () =>
+    imageSettings.doFlip("horizontal")
+  );
+  toolWrapper.append(flipVertButton, flipHorizButton);
+  toolList.appendChild(toolWrapper);
+};
+
+const scaleTool = (toolWrapper) => {
+  imageSettings.previousResolution = [...imageSettings.resolution];
+  const widthInput = document.createElement("input");
+  const heightInput = document.createElement("input");
+  const widthText = document.createElement("div");
+  const heightText = document.createElement("div");
+  widthText.textContent = "w";
+  heightText.textContent = "h";
+  widthInput.type = "number";
+  heightInput.type = "number";
+  widthInput.max = 7680;
+  widthInput.min = 0;
+  heightInput.max = 4320;
+  heightInput.min = 0;
+  widthInput.classList.add("app__scaleValue");
+  heightInput.classList.add("app__scaleValue");
+  widthInput.value = imageSettings.resolution[0];
+  heightInput.value = imageSettings.resolution[1];
+  widthInput.addEventListener("change", (e) => {
+    imageSettings.scale({ width: e.target.value });
+  });
+  heightInput.addEventListener("change", (e) => {
+    imageSettings.scale({ height: e.target.value });
+  });
+  toolWrapper.append(widthInput, widthText, heightInput, heightText);
   toolList.appendChild(toolWrapper);
 };
 
@@ -205,14 +254,13 @@ const openToolPanel = (tool) => {
   toggleToolButtons(false);
   switch (tool) {
     case "rotate":
-      {
-        renderTool(rotateTool, "Rotate");
-      }
+      renderTool(rotateTool, "Rotate");
       break;
     case "flip":
-      {
-        renderTool(flipTool, "Flip");
-      }
+      renderTool(flipTool, "Flip");
+      break;
+    case "scale":
+      renderTool(scaleTool, "Scale");
       break;
     default:
       break;
